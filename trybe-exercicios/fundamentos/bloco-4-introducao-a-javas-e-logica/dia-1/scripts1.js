6e97396d2a112a4aab8c90ba6bb82e2ef64d69c0
Anderson Sales <azkarsb1511@gmail.com>
2022-08-19 16:15:26 -0300

    exercicios de hoje

--- trybe-exercicios/fundamentos/bloco-4-introducao-a-javas-e-logica/dia-1/scripts1.js
+++ trybe-exercicios/fundamentos/bloco-4-introducao-a-javas-e-logica/dia-1/scripts1.js
@@ -5,9 +5,49 @@ const patientInfo = {
   lastName: 'Santos',
 };
 const patientEmail = 'ana@email.com';
-const base = 5;
-let height = 8;
-const area = base * height;
-const perimeter = base * 2 + height * 2;
-console.log(area);
-console.log(perimeter);
+
+
+
+const nota = 59;
+
+if (nota >= 80) {
+  console.log("Parabéns, você foi aprovada(o)!");
+}
+else if (nota <= 80 && nota >= 60){
+  console.log("Você está na nossa lista de espera");
+}
+else if (nota < 60){
+  console.log("Você foi reprovada(o)");
+}
+else {
+  console.log("Você foi reprovada(o)");
+}
+
+const currentHour = 7;
+let message;
+if (currentHour >= 22){
+  message = "Não deveríamos comer nada, é hora de dormir";
+}
+else if (currentHour >= 18 && currentHour < 22){
+  message = "Rango da noite, vamos jantar :D";
+}
+else if (currentHour >= 14 && currentHour < 18){
+  message = "Vamos fazer um bolo pro café da tarde?";
+}
+else if (currentHour >= 11 && currentHour < 14){
+  message = "Hora do almoço!!!";
+}
+else if(currentHour >= 4 && currentHour < 11){
+  message = "Hmmm, cheiro de café recém passado";
+}
+console.log (message);
+
+let weekDay = "sabado";
+
+if (weekDay == "segunda-feira" || weekDay == "terça-feira" || weekDay == "quarta-feira" || weekDay == "quinta-feira" || weekDay == "sexta-feira")
+{
+  console.log ("Oba, mais um dia de aprendizado na Trybe >:D");
+}
+else{
+  console.log("Feriado");
+}
